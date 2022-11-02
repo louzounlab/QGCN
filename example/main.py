f34648a39ceaa6ebca17c62bb15fbebc67a7b9fc
zivnaim <zivnaim3@gmail.com>
2022-11-02 11:05:24 +0200

    upload the QGCN library code and an example

---
+++ example/main.py
@@ -0,0 +1,15 @@
+from QGCN.activator import QGCNModel, QGCNDataSet
+from torch.utils.data import DataLoader
+
+qgcn_model = QGCNModel(dataset_name="Aids", params_file="./params/default_binary_params.json")
+qgcn_model.train()
+
+ds = QGCNDataSet("Aids", params_file="./params/default_binary_params.json")
+loader = DataLoader(
+    ds.get_dataset(),
+    shuffle=False
+)
+
+for _, (A, x0, embed, label) in enumerate(loader):
+    output = qgcn_model.predict(A, x0, embed)
+    print(output, label)
